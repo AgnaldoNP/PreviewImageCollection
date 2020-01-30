2209825bb1243f6187a1935d02a9890d7161bcf1
Agnaldo N. Pereira <agnaldo.pereira@etec.sp.gov.br>
2020-01-29 22:31:29 -0300

    fix build command

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -15,8 +15,8 @@ pipeline {
         stage('Unit & Integration Tests') {
             steps {
                 script {
-                    sh 'export ANDROID_SERIAL=0047758577 ; ./gradlew assemble'
-                    step([$class: 'ArtifactArchiver', artifacts: '**/app/build/outputs/apk/debug/app-debug.apk'])
+                    sh 'export ANDROID_SERIAL=0047758577 ; ./gradlew assemble ; ls app/build/outputs/apk/debug/'
+//                    step([$class: 'ArtifactArchiver', artifacts: 'app/build/outputs/apk/debug/app-debug.apk'])
 
                 }
             }
